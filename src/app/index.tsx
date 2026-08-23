8ab67359d700767bc6fbead48e039c4f9ab5c2a0
Ranjeet Singh <117429838+GG-Ranjeet@users.noreply.github.com>
2026-08-23 21:43:03 +0530

    chore: update dependencies and configure Tailwind CSS with NativeWind
    - Added babel-preset-expo and nativewind dependencies to package.json
    - Imported global CSS in _layout.tsx for Tailwind styles
    - Updated index.tsx to use Tailwind classes
    - Added nativewind-env.d.ts for TypeScript support
    - Created babel.config.js for Babel configuration with NativeWind
    - Added global.css for Tailwind directives
    - Configured metro.config.js to integrate NativeWind with Expo
    - Created tailwind.config.js for Tailwind CSS configuration

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,7 +3,9 @@ import { Text, View, StyleSheet } from "react-native";
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+      <Text
+      className="bg-slate-200"
+      >Edit src/app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
